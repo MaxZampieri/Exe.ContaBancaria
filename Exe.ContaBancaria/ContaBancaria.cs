3083f50c9821fe9e2622c3df60b75533f0af03f6
Max Zampieri <maxwzampieri@gmail.com>
2022-01-25 18:08:32 -0300

    projeto finalizado

---
+++ Exe.ContaBancaria/ContaBancaria.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+
+
+namespace Exe.ContaBancaria
+{
+    class ContaBancaria
+    {
+        public int Numero { get; private set; }
+        public string Titular { get; set; }
+        public double Saldo { get; private set; }
+
+        public ContaBancaria(int numero, string titular)
+        {
+            Numero = numero;
+            Titular = titular;
+            
+        }
+        public ContaBancaria(int numero, string titular, double saldo) : this( numero, titular)
+        {
+            Saldo = saldo;
+        }
+        public override string ToString()
+        {
+            return "Conta " + Numero + ", Titular: " + Titular + ", Saldo: R$ " 
+                + Saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+        public void Deposito (double quantia)
+        {
+            Saldo += quantia;
+        }
+        public void Saque (double quantia)
+        {
+            Saldo -= quantia + 5.0;
+        }
+    }
+}
---
+++ Exe.ContaBancaria/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exe.ContaBancaria
+{
+    class Program
+    {
+        static void Main(string[] args)
+            
+        {
+            ContaBancaria conta;
+
+            Console.Write("Entre com o numero da conta: ");
+            int numero = int.Parse(Console.ReadLine());
+            Console.Write("Entre com o titular da conta: ");
+            string titular = Console.ReadLine();
+            Console.Write("Haverá depósito (s/n) ? ");
+            char resp = char.Parse(Console.ReadLine()); 
+            if (resp == 's' || resp == 'S')
+            {
+                Console.WriteLine("Entre com o valor inicial: ");
+                double depositoInicial = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                conta = new ContaBancaria(numero, titular, depositoInicial);
+            }
+            else
+            {
+                conta = new ContaBancaria(numero, titular);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Dados da conta: ");
+            Console.WriteLine(conta);
+
+            Console.WriteLine();
+            Console.Write("Entre com um valor para deposito: ");
+            double quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.Deposito(quantia);
+            Console.WriteLine("Dados da conta atualizados: ");
+            Console.WriteLine(conta);
+
+            Console.WriteLine();
+            Console.Write("Entre com um valor para saque: ");
+            quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.Saque(quantia);
+            Console.WriteLine("Dados da conta atualizados: ");
+            Console.WriteLine(conta);
+        }
+    }
+    
+}
